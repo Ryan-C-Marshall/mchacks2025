fee3359125092f369d96b9c5d6660685bf6e05de
Mths-Lra <mathishugolara@gmail.com>
2025-01-26 06:33:14 -0500

    Flask operational

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
+import pandas as pd
 from backend.AI_Integration import ai_function
-
 app = Flask(__name__, template_folder='frontend', static_folder="frontend")
 CORS(app, origins=["http://127.0.0.1:5500"])
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/process-prompt', methods=['POST'])
 def process_prompt():
@@ -23,20 +24,19 @@ def process_prompt():
         # Pass the prompt to your AI processing function
         ai_response = ai_function(prompt)
         # Cut it to 30 songs
-        ai_array = ai_response[:30]
-          
-        print("Playlist: " + ai_array)
-
-        # Return the AI response as a JSON response
-        response = {
-            "message": ai_array,
-            "status": "success"
-        }
-
-
-        return jsonify(response)  # Send a JSON response back to the frontend
+        
+        ai_response = (ai_function(prompt).head(30)).astype(str)
+        filtered_response = ai_response[["Track Name", "Artist Name(s)"]]
+        to_return = filtered_response.to_dict(orient="records")
+        return jsonify(to_return)
+    
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+    
